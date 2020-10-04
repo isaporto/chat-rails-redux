b231771382638b787478e1124e1d2f599ed756e9
isabela porto <isamporto@hotmail.com>
2020-10-03 21:57:15 -0300

    Added faker gem and I created seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts "Destroying all User instances. Please wait"
+User.destroy_all
+puts "Destroying all Message instances. Please wait"
+Message.destroy_all
+puts "Destroying all Channel instances. Please wait"
+Channel.destroy_all
+puts "\n"
+puts "Creating some new Channel instances. One second, please"
+general = Channel.create(name: "general")
+brasil = Channel.create(name: "brasil")
+react = Channel.create(name: "react")
+puts "\n"
+puts "Creating some new User instances. One second, please"
+isa =   User.create(email: "isaportinho@hotmail.com", password: "senha123", username: "hisapudine")
+kleb =  User.create(email: "klebinho@uol.com.br", password: "senha123", username: "keblinho123")
+pablo = User.create(email: "pablo@yahoo.com.br", password: "senha123", username: "pablito")
+puts "\n"
+puts "Creating some random Message instances. One second, please"
+users = [isa, kleb, pablo]
+10.times do
+  Message.create(content: Faker::TvShows::BojackHorseman.quote, user: users.sample, channel: general)
+end
+10.times do
+  Message.create(content: Faker::TvShows::RickAndMorty.quote, user: users.sample, channel: brasil)
+end
+10.times do
+  Message.create(content: Faker::TvShows::SouthPark.quote, user: users.sample, channel: react)
+end
